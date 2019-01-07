6b09e360e2508863d94f78a4cd7a85e651f432ce
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-07 22:59:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -203,3 +203,23 @@ def player_numbers(team)
   end
   numbers
 end
+
+def num_points_scored(player)
+  points = 0
+  game_hash.each do |location, data|
+    data.each do |key, value|
+      if key == :players
+        value.each do |player_hash, data|
+          if player_hash == player
+            data.each do |statistic, num|
+              if statistic == :points
+                points = num
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+  points
+end
