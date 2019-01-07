bfd390c54cdf217bc779380d4ae1f152f8338ed7
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-07 22:50:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -162,7 +162,7 @@ def team_colors(team)
   game_hash.each do |location, data|
     data.each do |key, value|
       if value == 'Brooklyn Nets'
-        game_hash[location][:colors].fetch_values
+        game_hash[location][:colors]
       end
     end
   end
