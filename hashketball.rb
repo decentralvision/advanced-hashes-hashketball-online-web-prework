1528fe36c03b6fd6468cabbbc435d1b5b568c6a3
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-07 22:43:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -83,7 +83,7 @@ def game_hash
           :blocks => 15,
           :slam_dunks => 10
         },
-        'Desagna Diop' => {
+        'DeSagna Diop' => {
           :number => 2,
           :shoe => 14,
           :points => 24,
